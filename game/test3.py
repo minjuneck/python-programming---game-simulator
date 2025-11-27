e98ccf03acf9fe92d0fb8a545a6969dd12e8aa34
이민준 <143801657+minjuneck@users.noreply.github.com>
2025-11-27 15:55:16 +0900

    Add files via upload

--- game/test3.py
+++ game/test3.py
@@ -1,18 +1,22 @@
 import sys,os,subprocess,pickle,random
-
+from io import BytesIO
 
 try:
     import pygame
 except ImportError:
     print("pygame이 설치되어 있지 않습니다. 자동으로 설치를 시도합니다...")
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "pygame"])
-        import pygame
-    except Exception as e:
-        print("pygame 설치 실패:", e)
-        sys.exit(1)
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pygame"])
+    import pygame
 #pygame 설치
+try:
+    import requests
+    print("requests 이미 설치됨")
 
+except ImportError:
+    print("requests 없음 → 설치 시도 중...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+    import requests
+#request 설치
 
 
 pygame.init()  # pygame 초기화
@@ -20,6 +24,7 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((1200, 700))  # 창 크기 설정
 pygame.display.set_caption("기숙사에서 몰폰 잡기 시뮬레이션")  # 창 제목 설정
 clock = pygame.time.Clock()  # FPS(초당 프레임 수) 관리용
+
 
 
 #저장기능들
@@ -45,51 +50,69 @@ loaddata()
 textnum =0 #대화번호
 scripts = ["학생1: 404호 현원 다섯명 **이 안왔습니다","학생2: 405호 6명 이상무","나:어 그래 밤에 핸드폰 하지 말고, 점호 끝!"
            ,"학생들:와와아!","","(분명 몰래 폰하는 학생들이 있을 것이다,한번 검거해 보자)","(문을 클릭해 호실 들어가기)"
-           ,"...............","(폰을 하고 있는 학생을 클릭해 검거하자!)","다 잡았다"]
-endscripts = ["나는 학생들의 몰폰을 잡으려고 했지만, 아무런 수확이 없었다.","그 이후로 나는 학생들에게 만만하게 여겨져 학생들이 마구잡이로 룰을 어기게 되었다.","엔딩1:몰폰의 시대"
-              ,"1","2","엔딩2끝"]
+           ,"...............","(폰을 하고 있는 학생을 클릭해 검거하자!)","튜토리얼 끝"]
+endscripts = ["나는 학생들의 몰폰을 잡으려고 했지만, 아무런 수확이 없었다.","그 이후로 나는 학생들에게 만만하게 여겨져 학생들이 마구잡이로 룰을 어기게 되었다.","엔딩1:몰폰의 시대(배드엔딩)"
+              ,"어느정도 잡긴 했지만 많이 놓쳤다","뭐 애들이 어느정도는 경각심을 갖겠지","엔딩2 평범한 하루(노멀엔딩)",
+              "꽤 많이 잡았네","이젠 앞으로 몰폰 할 생각은 안하겠지","엔딩3 몰폰의 몰락(노멀엔딩)",
+              "이렇게 몰폰하는 학생이 많았어?","이제 더이상은 못한다","엔딩4 몰폰의 끝(노멀엔딩)",
+              "몰폰(히든엔딩)"]
 curscript = ""#출력될 대화
 text_index = 0#한 대화의 문자열 번호
 delay = 75#문자출력간 시간
 last_time = 0#전 타자시간,전시간
+tugging=False#힘쌘학생전용
+tugpoint=0
+curtugstudent=-1
 istalking=True#대화중인지
 storynum = 0 #스토리넘버
 
 students = [[3,3,1,3,1,3]]#0:비어있음 1: 자고있음1, 2:자고있음2, 3:당당히 폰함, 4:갑자기 숨김,5:이불속에서 함,
-#7:갑자기 숨김변화후이미지,6:중간에 도망침,8:도망침이미지,9:도망침자리이미지
-# 11:반항아(힘으로 이겨야함(광클),지면 도망) / 그후로는 뇌절생각중 9:음악러(리듬게임) 10:전교1등(퀴즈맞추기) 11:포켓몬 마스터...
+#10:갑자기 숨김변화후이미지,6:중간에 도망침,11:도망침이미지,12:도망침자리이미지
+# 7:힘쌘학생(힘으로 이겨야함(광클),지면 도망) / 그후로는 뇌절생각중 8:음악러(리듬게임) 10:전교1등(퀴즈맞추기) 11:포켓몬 마스터...
 studentimg = ["student2.png","student2.png","student3.png","student1.png","student1.png","student4.png",
-              "student5.png","student2.png","student5-1.png","student5-1.png"]#학생이미지
+              "student5.png","student6.png","student1.png","student1.png","student2.png","student5-1.png","student5-1.png"]#학생이미지
 
 doors = [0,0,0,0,0,0,0,0,0,0]#문상태
 curdoor=0
 running = True
-wholestream=0#0>튜토리얼 1>메인화면 2>설명 3>로비 4>호실안 5>결과 출력,엔딩
+wholestream=0#0>튜토리얼 1>메인화면 2>설명 3>로비 4>호실안 5>결과 출력,엔딩 6>히든 학생
 explainpage=0#로비페이지
-studentsexplain = ["그저 비어있음","자고 있는 착한 학생(잡으면 -2)","이불속에서 자고 있는 착한 학생(잡으면 -2)","사감쌤이 왔는데도 당당히 폰하는 학생(클릭해 잡기)",
-                   "숨기는 학생(시간이 지나면 자는 학생처럼 겉모습이 바뀜,기억했다가 잡기)","이불속에서 하는 학생(그냥잡기)"]
+studentsexplain = ["그저 비어있음","자고 있는 학생(잡으면 -2)","이불속에서 자고 있는 학생(잡으면 -2)","사감쌤이 왔는데도 당당히 폰하는 학생(클릭해 잡기)",
+                   "숨기는 학생(시간이 지나면 자는 학생처럼 겉모습이 바뀜,기억했다가 잡기)","이불속에서 하는 학생(그냥잡기)","도망치는 학생(시간이 지나면 화면 왼쪽으로 도망감,도망가는 것을 클릭해 잡기)","힘쌘 학생(누르고 광클해서 잡기 게이지가 없어지면 놓침)"]
 sounds = ["death.wav","brah.wav","pew.mp3","byam.mp3","afew.mp3"]
 score=0
-
+def setimage(a):
+    url="https://github.com/minjuneck/python-programming---game-simulator/blob/main/game/"+a
+    img_bytes = requests.get(url).content
+    img = pygame.image.load(BytesIO(img_bytes))
+    return img
 #함수존
 def set_studentimg(num,x,y):
-    img = pygame.image.load(studentimg[num])
-    if(num!=0 and num!=9):
+    img = setimage(studentimg[num])
+    if(num!=0 and num!=12):#111111111111111111111111111111111
         screen.blit(img, (x, y))
 
 def get_score(whereclick):
     global score
+    global tugging
+    global curtugstudent
+    global tugpoint
+    global wholestream
     if students[curdoor][whereclick]>2:
-        if students[curdoor][whereclick]!=9:
+        if students[curdoor][whereclick]!=12 and students[curdoor][whereclick]!=7 and students[curdoor][whereclick]!=8 and students[curdoor][whereclick]!=9:#111111111111111111111
             score+=1
             students[curdoor][whereclick]=0
-            pygame.mixer.music.load(sounds[random.randint(0, 3)])
-            pygame.mixer.music.play()
+            pygame.mixer.Sound(sounds[random.randint(0, 3)]).play()
+        elif(students[curdoor][whereclick]==7):
+            tugging=True
+            tugpoint =10
+            curtugstudent=whereclick
+        elif(students[curdoor][whereclick]==8 or students[curdoor][whereclick]==9):
+            wholestream = 6
     elif students[curdoor][whereclick]<=2 and students[curdoor][whereclick]>0:
         score-=2
         students[curdoor][whereclick]=0
-        pygame.mixer.music.load(sounds[random.randint(0, 3)])
-        pygame.mixer.music.play()
+        pygame.mixer.Sound(sounds[random.randint(0, 3)]).play()
 
 def opendoor(num):
         global doors
@@ -103,6 +126,8 @@ def opendoor(num):
             last_time =pygame.time.get_ticks()
 
 while running:#무한함수 > 실행중
+    pygame.mixer.music.load("brah.wav")
+    pygame.mixer.music.play()#bgm
     font = pygame.font.Font("C:/Windows/Fonts/malgun.ttf", 30)#기본폰트
     if data.get('튜토리얼')==1 and wholestream==0:
         wholestream=1
@@ -120,7 +145,9 @@ while running:#무한함수 > 실행중
             running = False#게임종료
 
         if event.type == pygame.MOUSEBUTTONDOWN:# 마우스 클릭
-            if(wholestream==0):
+            if(tugging):
+                tugpoint+=7
+            elif(wholestream==0):
                 if(text_index < len(scripts[textnum])):#대화출력중일시
                     text_index = len(scripts[textnum])#대화=full대화
                     curscript = scripts[textnum]
@@ -159,8 +186,7 @@ while running:#무한함수 > 실행중
                         text_index=0
                         if(textnum==4):
                             storynum+=1
-                            pygame.mixer.music.load(sounds[4])
-                            pygame.mixer.music.play()
+                            pygame.mixer.Sound(sounds[4]).play()
                         if(textnum==6):
                             storynum+=1
                         if(textnum==7):
@@ -169,7 +195,19 @@ while running:#무한함수 > 실행중
                 x, y = pygame.mouse.get_pos()
                 if x>300 and x<900 and y>525 and y<675:#game시작
                     wholestream=3
-                    students = [[random.randint(0, 6) for _ in range(6)] for _ in range(10)]
+                    students = [[random.randint(0, 2) for _ in range(6)] for _ in range(10)]
+                    for i in range(40):
+                        while 1:
+                            j,k=random.randint(0, 9),random.randint(0, 5)
+                            if students[j][k] <= 2:
+                                students[j][k] = random.randint(3, 7)
+                                break
+                    while 1:
+                        j,k=random.randint(0, 9),random.randint(0, 5)
+                        if students[j][k] <= 2:
+                            students[j][k] = random.randint(8, 9)
+                            break
+
                     print(students)
                 if x>1050 and x<1200 and y<200:
                     wholestream=2
@@ -177,7 +215,7 @@ while running:#무한함수 > 실행중
                 x, y = pygame.mouse.get_pos()
                 if x>1050 and x<1200 and y<200:
                     wholestream=1
-                if x>1050 and x<1200 and explainpage<5:
+                if x>1050 and x<1200 and explainpage<7:
                     explainpage+=1
                 if x>0 and x<150 and explainpage>0:
                     explainpage-=1
@@ -207,36 +245,32 @@ while running:#무한함수 > 실행중
                 x, y = pygame.mouse.get_pos()
                 if pygame.time.get_ticks() - last_time >= 700:
                     for i in range(6):
-                        if students[curdoor][i]==9:
+                        if students[curdoor][i]==12:##########
                             a = 20+400*int(i/2)-( pygame.time.get_ticks() - last_time - 700)/2
-                            if x> a and x<a+200 and y>500:
+                            if x> a and x<a+200 and y>550:
                                 score+=1
                                 students[curdoor][i]=0
-                                pygame.mixer.music.load(sounds[random.randint(0, 3)])
-                                pygame.mixer.music.play()
+                                pygame.mixer.Sound(sounds[random.randint(0, 3)]).play()
                 if x<350 and y<300:#1
                     get_score(0)
-                elif x<350 and y>300 and y<500:#2
+                elif x<350 and y>300 and y<550:#2
                     get_score(1)
                 elif x>350 and x<700 and y<300:#3
                     get_score(2)
-                elif x>350 and x<700 and y>300 and y<500:#4
+                elif x>350 and x<700 and y>300 and y<550:#4
                     get_score(3)
                 elif x>700 and x<1050 and y<300:#5
                     get_score(4)
-                elif x>700 and x<1050 and y>300 and y<500:#6
+                elif x>700 and x<1050 and y>300 and y<550:#6
                     get_score(5)
                 elif x>1050 and x<1200 and y<200:
                     wholestream=3
-                    '''for i in range(6):#다검거했는지 확인
-                        if students[curdoor][i]>2:
-                            break
-                        if i==5:'''
+
             elif(wholestream==5):
                 if(text_index < len(endscripts[textnum])):#대화출력중일시
                     text_index = len(endscripts[textnum])#대화=full대화
                     curscript = endscripts[textnum]
-                elif textnum ==2 or textnum==5:#더이상의 대화 없으면,대화끊기
+                elif textnum ==2 or textnum==5 or textnum==8 or textnum==11:#더이상의 대화 없으면,대화끊기
                     curscript=""
                     istalking = False
                 else:#다음대화 넘어가야 할때
@@ -246,23 +280,24 @@ while running:#무한함수 > 실행중
 
 
 
+
     #화면 그리기
 
 
 
     if(wholestream==0):
         if(storynum==0):
-            img = pygame.image.load("background1.png")  # 이미지 불러오기
+            img = setimage("background1.png")  # 이미지 불러오기
             screen.blit(img, (0, 0))
         elif(storynum==1):
-            img = pygame.image.load("afew.png")  # 이미지 불러오기
+            img = setimage("afew.png")  # 이미지 불러오기
             screen.blit(img, (0, 0))
         elif(storynum==2):
-            img = pygame.image.load("background2.png")  # 이미지 불러오기
+            img = setimage("background2.png")  # 이미지 불러오기
             screen.blit(img, (0, 0))
         elif(storynum==3):
             screen.fill((255,255,255))
-            img = pygame.image.load("bed.png")  # 이미지 불러오기
+            img = setimage("bed.png")  # 이미지 불러오기
             screen.blit(img, (10, 100))
             screen.blit(img, (410, 100))
             screen.blit(img, (810, 100))
@@ -275,7 +310,7 @@ while running:#무한함수 > 실행중
             text = font.render(str(score), True, (0, 0, 0))  # 글자 렌더링
             screen.blit(text, (1100, 0))  # 출력
         if istalking:
-            img = pygame.image.load("pannel.png")  # 이미지 불러오기
+            img = setimage("pannel.png")  # 이미지 불러오기
             screen.blit(img, (100, 575))
         #pygame.draw.rect(screen, (0,0,255), (100, 575, 1000, 100))  # 사각형
 
@@ -287,12 +322,12 @@ while running:#무한함수 > 실행중
                 curscript += scripts[textnum][text_index]
                 text_index += 1
                 last_time = now
-
+                pygame.mixer.Sound("pop.mp3").play()
 
             text = font.render(curscript, True, (0, 0, 0))  # 글자 렌더링
             screen.blit(text, (140, 600))  # 출력
     elif wholestream==1:
-        img = pygame.image.load("background3.png")  # 이미지 불러오기
+        img = setimage("background3.png")  # 이미지 불러오기
         screen.blit(img, (0, 0))
     elif wholestream==2:
         screen.fill((255,255,255))
@@ -308,30 +343,34 @@ while running:#무한함수 > 실행중
         text = font.render("메인화면으로", True, (0, 0, 0))  # 글자 렌더링
         screen.blit(text, (1000, 50))  # 출력
     elif wholestream==3:
-        img = pygame.image.load("background4.png")  # 이미지 불러오기
+        img = setimage("background4.png")  # 이미지 불러오기
         screen.blit(img, (0, 0))
-        img = pygame.image.load("openeddoor.png")
+        img = setimage("openeddoor.png")
         for i in range(5):
             if doors[i]==1:
-                screen.blit(img, (82+219*i, 42))
+                screen.blit(img, (80+220*i, 40))
         for i in range(5,10):
             if doors[i]==1:
-                screen.blit(img, (82+219*(i-5), 383))
+                screen.blit(img, (80+220*(i-5), 380))
         for i in range(10):
             if doors[i]==0:
                 break
             if i==9:
-                if score<0:
+                if score<=5:
                     textnum=0
-                elif score < 10:
+                elif score <= 20:
                     textnum=3
+                elif score <=40:
+                    textnum=6
+                else:
+                    textnum=9
                 wholestream=5
                 istalking=True
         text = font.render(str(score), True, (0, 0, 0))  # 글자 렌더링
         screen.blit(text, (1100, 0))  # 출력
     elif wholestream==4:
         screen.fill((255,255,255))
-        img = pygame.image.load("bed.png")  # 이미지 불러오기
+        img = setimage("bed.png")  # 이미지 불러오기
         screen.blit(img, (10, 100))
         screen.blit(img, (410, 100))
         screen.blit(img, (810, 100))
@@ -343,19 +382,20 @@ while running:#무한함수 > 실행중
         set_studentimg(students[curdoor][5],820,350)
         text = font.render(str(score), True, (0, 0, 0))  # 글자 렌더링
         screen.blit(text, (1100, 0))  # 출력
-        img = pygame.image.load("out.png")  # 이미지 불러오기
+        img = setimage("out.png")  # 이미지 불러오기
         screen.blit(img, (1000, 50))
         if pygame.time.get_ticks() - last_time >= 500:
             for i in range(6):
                 if students[curdoor][i]==4:
-                    students[curdoor][i]=7
+                    students[curdoor][i]=10########
         if pygame.time.get_ticks() - last_time >= 700:
             for i in range(6):
-                if students[curdoor][i]==6 or students[curdoor][i]==9:
-                    students[curdoor][i]=9
-                    set_studentimg(8,20+400*int(i/2)-( pygame.time.get_ticks() - last_time - 700)/2,500)
+                if students[curdoor][i]==6 or students[curdoor][i]==12:##########
+                    students[curdoor][i]=12
+                    set_studentimg(11,20+400*int(i/2)-( pygame.time.get_ticks() - last_time - 700)/2,550)
     elif wholestream==5:
-        screen.fill((255,255,255))
+        img = setimage("background1.png")  # 이미지 불러오기
+        screen.blit(img, (0, 0))
         text = font.render(str(score), True, (0, 0, 0))  # 글자 렌더링
         screen.blit(text, (1100, 0))  # 출력
         if istalking:
@@ -365,9 +405,30 @@ while running:#무한함수 > 실행중
                 curscript += endscripts[textnum][text_index]
                 text_index += 1
                 last_time = now
+                pygame.mixer.Sound("pop.mp3").play()
 
             text = font.render(curscript, True, (0, 0, 0))  # 글자 렌더링
             screen.blit(text, (140, 600))  # 출력
+    elif wholestream==6:
+        img = setimage("background1.png")  # 이미지 불러오기
+        screen.blit(img, (0, 0))
+        text = font.render("여긴 히든 룸ㅁㅁㅁㅁ", True, (0, 0, 0))  # 글자 렌더링
+        screen.blit(text, (1100, 0))  # 출력
+    if(tugging):
+        pygame.draw.rect(screen, (0,0,0), (100, 50,1000,150))
+        pygame.draw.rect(screen, (0,0,255), (100, 50,10*tugpoint,150))
+        if(pygame.time.get_ticks() % 3==0):
+            tugpoint-=1
+        if(tugpoint>=100):
+            tugging=0
+            tugging=False
+            score+=1
+            students[curdoor][curtugstudent]=0
+            curtugstudent=-1
+            pygame.mixer.Sound(sounds[random.randint(0, 3)]).play()
+        elif(tugpoint<0):
+            tugging=0
+            students[curdoor][curtugstudent]=0
     pygame.display.flip()  # 화면 업데이트
 
     #FPS 제한
